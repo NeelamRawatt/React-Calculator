2fef7309337bbc3d797a9833a43b66d5b21f360c
neelam <neelam30rawat@gmail.com>
2022-07-16 11:45:13 +0530

    first commit

---
+++ src/DigitButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ACTIONS } from "./App";
+
+// dispatch so that we can call a reducer from here
+const DigitButton = ({ dispatch, digit }) => {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+};
+
+export default DigitButton;
